53b04e2feb0df88fd57141d43da1338554e70129
Apollo666-ex <80152585+Apollo666-ex@users.noreply.github.com>
2021-04-07 12:51:28 +0000

    alvingans24.py

--- userbot/modules/alvingans24.py
+++ userbot/modules/alvingans24.py
@@ -10,36 +10,25 @@ import asyncio
 @register(outgoing=True, pattern="^.hua$")
 async def koc(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("أ‿أ")
-        await e.edit("╥﹏╥")
-        await e.edit("(;﹏;)")
-        await e.edit("(ToT)")
-        await e.edit("(┳Д┳)")
-        await e.edit("(ಥ﹏ಥ)")
-        await e.edit("（；へ：）")
-        await e.edit("(T＿T)")
-        await e.edit("（πーπ）")
-        await e.edit("(Ｔ▽Ｔ)")
-        await e.edit("(⋟﹏⋞)")
-        await e.edit("（ｉДｉ）")
-        await e.edit("(´Д⊂ヽ")
-        await e.edit("(;Д;)")
-        await e.edit("（>﹏<）")
-        await e.edit("(TдT)")
-        await e.edit("(つ﹏⊂)")
-        await e.edit("༼☯﹏☯༽")
-        await e.edit("(ノ﹏ヽ)")
-        await e.edit("(ノAヽ)")
-        await e.edit("(╥_╥)")
-        await e.edit("(T⌓T)")
-        await e.edit("(༎ຶ⌑༎ຶ)")
-        await e.edit("(☍﹏⁰)｡")
-        await e.edit("(ಥ_ʖಥ)")
-        await e.edit("(つд⊂)")
-        await e.edit("(≖͞_≖̥)")
-        await e.edit("(இ﹏இ`｡)")
-        await e.edit("༼ಢ_ಢ༽")
-        await e.edit("༼ ༎ຶ ෴ ༎ຶ༽")
+        await e.edit("`\n{\__/}`"
+                     "`\n( • . •)`"   
+                     "`\n/>❤️ take, this is for you`")
+        sleep(3)
+        await typew.edit("`\n{\__/}`"
+                         "`\n( • - •)`"  
+                         "`\n  ❤️ < \  wait!!! this is little`")
+        sleep(2)
+        await typew.edit("`\n{\__/}`"
+                         "`\n( • . •) take to much more`"
+                         "`\n/ >💕💓💓
+                                 💓💓💓
+                                💓💓💓💓
+                              💓💓💓💓💓💓
+                            💓💓💓💓💓💓💓💓
+                          💓💓💓💓💓💓💓💓💓💓`"
+      
+     
+
 
 
 @register(outgoing=True, pattern='^.huh(?: |$)(.*)')
